df23855fa4363946c16f2d6057cd24015da0b5f6
cioby09 <claudiociobanu09@gmail.com>
2023-04-19 22:31:35 +0200

    Add: AppTitle component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script>
+import AppTitle from "./components/AppTitle.vue"
+
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppTitle />
+</template>
 
 <style></style>
